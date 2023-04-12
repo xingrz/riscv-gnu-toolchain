b67ef84513aabb9036052f34a42344b1deeb2c1e
Huaqi Fang <578567190@qq.com>
2023-04-12 19:26:53 +0800

    script: update submodule don't exit
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- scripts/toolchain/prepsrc.sh
+++ scripts/toolchain/prepsrc.sh
@@ -67,7 +67,8 @@ function git_clone_repo() {
 function git_submodule_update() {
     echo "INFO: Init and update repo $(basename $(pwd)) submodule"
     git submodule sync
-    git submodule update -f --init --recursive ${gitopts} || echo "ERROR: Update submodule fail"; exit 1
+    git submodule update -f --init --recursive ${gitopts}
+    git submodule || echo "ERROR: Update submodule fail"; exit 1
 }
 
 function init_toolchain_repo() {
